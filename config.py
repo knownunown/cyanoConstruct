6ba7a3c4a197eab030a5e28543ee5d3e8a0ea12f
Lia X Thomson <lialovespets@gmail.com>
2020-05-20 20:48:43 -0500

    re-inserted config

---
+++ config.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May  3 15:27:32 2020
+
+@author: Lia Thomson
+
+cyanoConstruct config file
+"""
+import os
+
+class Config(object):
+    SECRET_KEY = "Salix babylonica crispa" #should get a better secret key
+    #SQLALCHEMY_DATABASE_URI = "sqlite:////tmp/test42.db"
+    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(os.path.abspath(os.path.dirname(__file__)), 'cyano.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
