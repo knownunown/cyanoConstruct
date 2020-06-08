e772f94e3ec4e756844d04f31bf455d1e7b470ff
Lia X Thomson <lialovespets@gmail.com>
2020-06-07 22:08:05 -0500

    database migrations

---
+++ migrations/versions/e102134316d4_.py
@@ -0,0 +1,31 @@
+"""empty message
+
+Revision ID: e102134316d4
+Revises: 9c5bf65fbb5e
+Create Date: 2020-06-07 16:15:02.420281
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'e102134316d4'
+down_revision = 'bd2054290318'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+        op.add_column('UserData', sa.Column('googleID', sa.TEXT(), nullable=True, server_default="False"))
+        op.add_column('UserData', sa.Column('googleAssoc', sa.BOOLEAN(), nullable=True))
+        op.create_unique_constraint(None, 'UserData', ['googleID'])
+        op.add_column('UserData', sa.Column('tempPass', sa.String(32), nullable = True))
+        op.add_column('UserData', sa.Column('tempExp', sa.INTEGER(), nullable = True))
+
+
+def downgrade():
+        op.drop_column('UserData', 'googleID')
+        op.drop_column('UserData', 'googleAssoc')
+        op.drop_column('UserData', 'tempPass')
+        op.drop_column('UserData', 'tempExp')
