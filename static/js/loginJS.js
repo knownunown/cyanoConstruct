81201f2f27329e912408d3d6fb83d91386be4e92
Lia X Thomson <lialovespets@gmail.com>
2020-06-19 17:06:35 -0500

    google sign-in testing 2.5.3

--- static/js/loginJS.js
+++ static/js/loginJS.js
@@ -57,8 +57,8 @@ function onSignIn(googleUser) {
 
 	var IDtoken = googleUser.getAuthResponse().id_token;
 
-	var loginData = "{'test': 'worked', 'IDtoken': \"\"\"" + IDtoken + "\"\"\", 'fullName': \"\"\"" + profile.getName() + "\"\"\", 'email': \"\"\"" + profile.getEmail() + 
-					"\"\"\", 'remember': '" + document.getElementById("googleRememberMe").checked.toString() + "''}";
+	var loginData = "{'test': 'worked', 'fullName': \"\"\"" + profile.getName() + "\"\"\", 'email': \"\"\"" + profile.getEmail() + 
+					"\"\"\", 'remember': '" + document.getElementById("googleRememberMe").checked.toString() + "', 'IDtoken': \"\"\"" + IDtoken + "\"\"\"}";
 
 	alert(loginData);
 
