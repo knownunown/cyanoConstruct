033b7247ab7be19ddd2eaa3bdd7ce6d3c46f816d
Lia X Thomson <lialovespets@gmail.com>
2020-06-19 19:38:09 -0500

    removed alert

--- static/js/loginJS.js
+++ static/js/loginJS.js
@@ -60,8 +60,6 @@ function onSignIn(googleUser) {
 	var loginData = "{'fullName': \"\"\"" + profile.getName() + "\"\"\", 'email': \"\"\"" + profile.getEmail() + 
 					"\"\"\", 'remember': '" + document.getElementById("googleRememberMe").checked.toString() + "', 'IDtoken': \"\"\"" + IDtoken + "\"\"\"}";
 
-	alert(loginData);
-
 	$.ajax({
 		data : {loginData: loginData},
 		type : 'POST',
