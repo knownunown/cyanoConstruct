affa734479e2a0669d17e57083aeb07d8ffda137
Andrew Pan <known@unown.me>
2021-04-09 01:01:55 -0500

    flake: gate python-language-server for linux

--- flake.nix
+++ flake.nix
@@ -7,13 +7,14 @@
   outputs = { self, nixpkgs, utils }: utils.lib.eachDefaultSystem
     (system:
       let pkgs = import nixpkgs { inherit system; };
+          linuxPkgs = if pkgs.stdenv.isLinux then [ pkgs.python-language-server ] else [];
       in {
         devShell = pkgs.mkShell {
           name = "cc-devshell";
 
           buildInputs = with pkgs; [
-            python3 python-language-server python3Packages.pylint poetry
-          ];
+            python3 python3Packages.pylint poetry
+          ] ++ linuxPkgs;
 
           nativeBuildInputs = with pkgs; [];
         };
