4d61ac6a9608adb20915bd5cc39d1d4a80974427
Andrew Pan <known@unown.me>
2021-07-11 17:30:41 -0500

    web/ConstructComponent: quick and ugly component carousel

--- web/src/components/ConstructComponent.vue
+++ web/src/components/ConstructComponent.vue
@@ -1,9 +1,9 @@
 <template>
   <figure
-    class="w-48 h-16"
+    class="w-48 h-16 p-4 m-4 rounded-md border-solid border-4 border-green-400"
     draggable="true"
   >
-    <h1>{{ name }} [{{ position.posType }}]</h1>
+    <span class="truncate">{{ name }} [{{ posTypeName }}]</span>
     <div class="flex flex-row">
       <span class="flex-grow transform -translate-y-12">{{ spacers.left }}</span>
       <span class="flex-grow">{{ spacers.right }}</span>
@@ -14,7 +14,8 @@
 import { Vue, Options } from 'vue-class-component';
 import { PropType } from 'vue';
 
-import { PositionInfo, Spacers } from '../types';
+import { PositionInfo, PositionType, Spacers } from '../types';
+
   
 @Options({
     props: {
@@ -24,6 +25,9 @@ import { PositionInfo, Spacers } from '../types';
     }
 })
 export default class ConstructComponent extends Vue {
-    
+    get posTypeName() {
+        // @ts-ignore
+        return Object.keys(PositionType).find(k => PositionType[k] == this.position.posType);
+    }
 }
 </script>
