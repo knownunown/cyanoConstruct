857172791647be2915bed0926bccdf5080d77000
Andrew Pan <known@unown.me>
2021-08-10 02:36:09 -0700

    flake: move mypy to poetry

--- flake.nix
+++ flake.nix
@@ -1,13 +1,13 @@
 {
   inputs = {
-    nixpkgs.url = "nixpkgs";
+    nixpkgs.url = "nixpkgs/release-21.05";
     utils.url = "github:numtide/flake-utils";
   };
 
   outputs = { self, nixpkgs, utils }: utils.lib.eachDefaultSystem
     (system:
       let pkgs = import nixpkgs { inherit system; };
-          linuxPkgs = if pkgs.stdenv.isLinux then [ pkgs.python-language-server ] else [];
+          linuxPkgs = if pkgs.stdenv.isLinux then [ /* pkgs.python-language-server */ ] else [];
       in {
         devShell = pkgs.mkShell {
           name = "cc-devshell";
@@ -18,7 +18,7 @@
             sqlite
 
             # backend
-            python3 python3Packages.pylint poetry mypy black
+            python3 python3Packages.pylint pyright poetry black
           ] ++ (with nodePackages; [
             # frontend
             nodejs npm typescript-language-server vls
